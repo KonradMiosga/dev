e951fd7bab629238627796c9a9f469e30c280c02
Konrad Miosga <konrad.miosga@gmail.com>
2025-05-13 21:27:07 +0200

    automated dev commit

---
+++ env/.config/qutebrowser/config.py
@@ -0,0 +1,66 @@
+config.load_autoconfig() # load settings done via the gui
+
+c.auto_save.session = True # save tabs on quit/restart
+
+config.bind('<Mod4-1>', 'tab-focus 1')
+config.bind('<Mod4-2>', 'tab-focus 2')
+config.bind('<Mod4-3>', 'tab-focus 3')
+config.bind('<Mod4-4>', 'tab-focus 4')
+config.bind('<Mod4-5>', 'tab-focus 5')
+
+config.bind('M', 'hint links spawn mpv {hint-url}')
+
+# dark mode setup
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
+c.colors.webpage.darkmode.policy.images = 'never'
+config.set('colors.webpage.darkmode.enabled', False, 'file://*')
+
+# # fonts
+# c.fonts.default_family = []
+# c.fonts.default_size = '13pt'
+# c.fonts.web.family.fixed = 'monospace'
+# c.fonts.web.family.sans_serif = 'monospace'
+# c.fonts.web.family.serif = 'monospace'
+# c.fonts.web.family.standard = 'monospace'
+#
+# # privacy - adjust these settings based on your preference
+# # config.set("completion.cmd_history_max_items", 0)
+# # config.set("content.private_browsing", True)
+# config.set("content.webgl", False, "*")
+# config.set("content.canvas_reading", False)
+# config.set("content.geolocation", False)
+# config.set("content.webrtc_ip_handling_policy", "default-public-interface-only")
+# config.set("content.cookies.accept", "all")
+# config.set("content.cookies.store", True)
+# # config.set("content.javascript.enabled", False) # tsh keybind to toggle
+#
+# # Adblocking info -->
+# # For yt ads: place the greasemonkey script yt-ads.js in your greasemonkey folder (~/.config/qutebrowser/greasemonkey).
+# # The script skips through the entire ad, so all you have to do is click the skip button.
+# # Yeah it's not ublock origin, but if you want a minimal browser, this is a solution for the tradeoff.
+# # You can also watch yt vids directly in mpv, see qutebrowser FAQ for how to do that.
+# # If you want additional blocklists, you can get the python-adblock package, or you can uncomment the ublock lists here.
+c.content.blocking.enabled = True
+# c.content.blocking.method = 'adblock' # uncomment this if you install python-adblock
+# c.content.blocking.adblock.lists = [
+#         "https://github.com/ewpratten/youtube_ad_blocklist/blob/master/blocklist.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2024.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
+#         "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"]
+
+config.source('./gruvbox.py')
