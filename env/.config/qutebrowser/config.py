46c83277b88f90bb5d86a7748872a02017b4901b
Konrad Miosga <konrad.miosga@gmail.com>
2025-05-19 11:56:46 +0200

    automated dev commit

--- env/.config/qutebrowser/config.py
+++ env/.config/qutebrowser/config.py
@@ -8,8 +8,10 @@ config.bind('<Mod4-3>', 'tab-focus 3')
 config.bind('<Mod4-4>', 'tab-focus 4')
 config.bind('<Mod4-5>', 'tab-focus 5')
 config.bind('xb', 'config-cycle statusbar.show always never')
-
 config.bind('M', 'hint links spawn mpv {hint-url}')
+
+# Show status bar by default
+config.set('statusbar.show', 'always')
 
 # dark mode setup
 c.colors.webpage.darkmode.enabled = True
