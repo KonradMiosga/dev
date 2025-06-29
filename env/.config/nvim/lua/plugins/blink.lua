53b446e889cfb43c832056ccafb96f6f51482141
Konrad Miosga <konrad.miosga@gmail.com>
2025-06-29 12:14:49 +0200

    automated dev commit

--- env/.config/nvim/lua/plugins/blink.lua
+++ env/.config/nvim/lua/plugins/blink.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    "saghen/blink.cmp",
-    opts = function(_, opts)
-      opts.keymap = {
-        preset = "none", -- Disable default keymap presets
-        ["<C-y>"] = { "select_and_accept" }, -- Optional: bind Ctrl-Y to accept
-        -- Note: <CR> is intentionally omitted to disable it
-      }
-    end,
-  },
+  -- {
+  --   "saghen/blink.cmp",
+  --   opts = function(_, opts)
+  --     opts.keymap = {
+  --       preset = "none", -- Disable default keymap presets
+  --       ["<C-y>"] = { "select_and_accept" }, -- Optional: bind Ctrl-Y to accept
+  --       -- Note: <CR> is intentionally omitted to disable it
+  --     }
+  --   end,
+  -- },
 }
